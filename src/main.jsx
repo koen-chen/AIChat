736ab1f597758cc3d869d34ed351b502f8cc2300
Koen <koen.node@gmail.com>
2023-10-10 16:17:08 +0800

    feat: update ui

--- src/main.jsx
+++ src/main.jsx
@@ -4,15 +4,15 @@ import Chat from './Chat.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <div className='h-full w-full flex flex-col py-10'>
-      <div className="text-center mb-5 w-full h-20">
+    <div className='h-full w-full flex flex-col'>
+      <div className="text-center mb-5 mt-10 w-full h-20">
         <img
           className="h-full inline-block"
           src="/ai.jpg"
         />
       </div>
 
-      <div className='px-5 lg:px-10 w-full h-full'>
+      <div className='lg:px-20 lg:pb-20 w-full h-full'>
          <Chat />
       </div>
     </div>
