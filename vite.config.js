6bca4ac363a2725f7bf181a02aa1d1ead700d45c
Koen <koen.node@gmail.com>
2023-10-24 21:22:43 +0800

    fix: fixed config

--- vite.config.js
+++ vite.config.js
@@ -6,29 +6,36 @@ import tailwindcss from  'tailwindcss'
 import autoprefixer from 'autoprefixer'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, '/src')
-    }
-  },
-  plugins: [
-    react()
-  ],
-  css: {
-    postcss: {
-      plugins: [
-        tailwindcss,
-        autoprefixer,
-      ]
-    }
-  },
-  server: {
-    port: 3000,
-    host: 'localhost',
-    https: {
-      key: fs.readFileSync(path.resolve(__dirname, 'ssl/l-mall.key')),
-      cert: fs.readFileSync(path.resolve(__dirname, 'ssl/l-mall.crt'))
+export default defineConfig(({ command }) => {
+  const config = {
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, '/src')
+      }
+    },
+    plugins: [
+      react()
+    ],
+    css: {
+      postcss: {
+        plugins: [
+          tailwindcss,
+          autoprefixer,
+        ]
+      }
     }
   }
+
+  if (command == 'serve') {
+    config.server = {
+      port: 3000,
+      host: 'localhost',
+      https: {
+        key: fs.readFileSync(path.resolve(__dirname, 'ssl/l-mall.key')),
+        cert: fs.readFileSync(path.resolve(__dirname, 'ssl/l-mall.crt'))
+      }
+    }
+  }
+
+  return config
 })
